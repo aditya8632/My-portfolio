1064dc54fc32bbfcfb4440d81f698f10b821cffb
adityawadje snippet <adityawadje863270@gmail.com>
2024-06-30 18:41:43 +0530

    Add files via upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:myemail@gmail.com">wadjeaditya59@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/aditya-wadje-6483b1267">
+            linkedin.com/Aditya Wadje
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/aditya8632">github.com/aditya8632</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
